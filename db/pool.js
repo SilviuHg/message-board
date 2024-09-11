905600ba3ac3e0de8d3cec916eaa3c056e83cc19
SilviuHg <hagisilviu09@gmail.com>
2024-09-11 20:32:15 +0300

    updated pool

--- db/pool.js
+++ db/pool.js
@@ -1,25 +1,10 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-console.log(
-  process.env.PG_HOST,
-  process.env.PG_USER,
-  process.env.PG_DATABASE,
-  process.env.PG_PASSWORD,
-  process.env.PG_PORT
-);
-
-console.log(
-  process.env.PGHOST,
-  process.env.PGUSER,
-  process.env.PGDATABASE,
-  process.env.PGPASSWORD,
-  process.env.PGPORT
-);
 module.exports = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
